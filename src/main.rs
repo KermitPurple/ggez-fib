249766d91fdb8843b508732cfee4e725eed36ce9
Shane McDonough <shanemcdman@gmail.com>
2021-08-09 13:42:44 -0700

    Add lines

--- src/main.rs
+++ src/main.rs
@@ -32,18 +32,54 @@ impl Game {
         }
     }
 
+    fn get_curve_points(&self, p: (Point, Point, Point), n_points: i32) -> Vec<Point> {
+        let mut res = vec![];
+        for i in 0..=n_points {
+            let t = i as f32 / n_points as f32;
+            res.push((p.0 * (1. - t) + p.1 * t) * (1. - t) + (p.1 * (1. - t) + p.2 * t) * t);
+        }
+        res
+    }
+
     fn draw_fib(&mut self, ctx: &mut Context) -> GameResult {
-        let (mut prev, mut curr) = (0., 30.);
+        let (mut prev, mut curr) = (0., 1.);
         let mut mb = graphics::MeshBuilder::new();
         let mut pos = Point::new(0., 0.);
         for i in 0..20 {
-            println!("{}", curr);
             let square = Rect::new(pos.x, pos.y , curr, curr);
-            let dpos = match i % 4 {
-                0 => Point::new(curr, 0.),
-                1 => Point::new(-prev, curr),
-                2 => Point::new(-curr - prev, -prev),
-                3 => Point::new(0., - curr - prev),
+            let (dpos, points) = match i % 4 {
+                0 => (
+                    Point::new(curr, 0.),
+                    (
+                        Point::new(pos.x, pos.y + curr),
+                        Point::new(pos.x, pos.y),
+                        Point::new(pos.x + curr, pos.y),
+                        )
+                    ),
+                1 => (
+                    Point::new(-prev, curr),
+                    (
+                        Point::new(pos.x, pos.y),
+                        Point::new(pos.x + curr, pos.y),
+                        Point::new(pos.x + curr, pos.y + curr),
+                        )
+                    ),
+                2 => (
+                    Point::new(-curr - prev, -prev),
+                    (
+                        Point::new(pos.x + curr, pos.y),
+                        Point::new(pos.x + curr, pos.y + curr),
+                        Point::new(pos.x, pos.y + curr),
+                        )
+                    ),
+                3 => (
+                    Point::new(0., - curr - prev),
+                    (
+                        Point::new(pos.x + curr, pos.y + curr),
+                        Point::new(pos.x, pos.y + curr),
+                        Point::new(pos.x, pos.y),
+                        )
+                    ),
                 _ => unreachable!(),
             };
             pos += dpos;
@@ -56,7 +92,13 @@ impl Game {
                     DrawMode::stroke(5.),
                     square,
                     Color::from_rgb(100, 100, 100),
+                )?
+                .line(
+                    &self.get_curve_points(points, 100)[..],
+                    5.,
+                    Color::from_rgb(100, 100, 100),
                 )?;
+
             prev += curr;
             std::mem::swap(&mut prev, &mut curr);
         }
